b05a1513ba07646caf7518930db33d74987043e1
Alex Ovsyannikov <xaonst@gmail.com>
2020-11-24 10:10:18 -0600

    Add nvm to exports

--- bash/exports.sh
+++ bash/exports.sh
@@ -1,2 +1,7 @@
 # Modify the path
 export PATH=$PATH:/mnt/a/Installed\ Programs/cbwin:/mnt/c/Windows/System32/:~/.local/bin:/usr/local/bin:/mnt/c/Users/Xaon/AppData/Roaming/npm:~/.vimpkg/bin
+
+# Node version manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
